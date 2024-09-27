5575a154684fec75c522aa9c3b5c731755ed1cc6
wdrikss <w.drik@outlook.com>
2024-09-26 23:36:49 -0300

    feat: add find by slug api

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,18 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async findBySlug(ctx) {
+    const { slug } = ctx.params;
+
+    const post = await strapi.db.query('api::post.post').findOne({
+      where: { slug },
+    });
+
+    if (!post) {
+      return ctx.throw(404, 'Post não encontrado');
+    }
+
+    return post;
+  },
+}));
--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,4 +6,27 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+const defaultRouter = createCoreRouter('api::post.post');
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: 'GET',
+    path: '/posts/slug/:slug',
+    handler: 'api::post.post.findBySlug',
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
