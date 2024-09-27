612b034e72e0a6e36b916b41a510f28ad9d96456
wdrikss <w.drik@outlook.com>
2024-09-27 00:18:15 -0300

    feat: add find by slug

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -10,12 +10,15 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
   async findBySlug(ctx) {
     const { slug } = ctx.params;
 
+    const populate = ctx.query.populate === '*' ? true : ctx.query.populate;
+
     const post = await strapi.db.query('api::post.post').findOne({
       where: { slug },
+      populate: populate || {},
     });
 
     if (!post) {
-      return ctx.throw(404, 'Post não encontrado');
+      return ctx.throw(404, 'Post not found');
     }
 
     return post;
